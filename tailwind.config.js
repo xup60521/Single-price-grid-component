9980899a475e49b52e703d0b9f5a4b4065888c39
xup60521 <84887662+xup60521@users.noreply.github.com>
2024-08-27 12:58:49 +0800

    finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,13 @@ export default {
     theme: {
         extend: {
             colors: {
-
+                p_Cyan: "hsl(179, 62%, 43%)",
+                p_Bright_Yellow: "hsl(71, 73%, 54%)",
+                p_Light_Gray: "hsl(204, 43%, 93%)",
+                p_Grayish_Blue: "hsl(218, 22%, 67%)",
             },
             fontFamily: {
-
+                karla: ["Karla", "sans-serif"]
             }
         },
     },
